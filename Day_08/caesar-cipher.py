f5b6b4d988f3138535627351403320351d9826cf
dawoud <mhmaddawoud20@gmail.com>
2023-08-21 20:17:22 +0300

    Fixing some bugs

--- Day_08/caesar-cipher.py
+++ Day_08/caesar-cipher.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            'v', 'w', 'x', 'y', 'z']
 
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -12,7 +11,7 @@ def caesar(start_text, shift_amount, cipher_direction):
 
     for char in start_text:
         if char in alphabet:
-            pos = alphabet.index(char) % 25 + shift_amount
+            pos = (alphabet.index(char) % 25 + shift_amount) % 25
             end_text += alphabet[pos]
         else:
             end_text += char
@@ -31,9 +30,11 @@ while not should_end:
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
+    # shift %= 25
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
     if restart == "no":
         should_end = True
         print("GO TO HELL")
+
