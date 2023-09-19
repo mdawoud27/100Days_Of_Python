11f5e894be02ff49829c28d9b62b3d3a507d0a77
dawoud <mhmaddawoud20@gmail.com>
2023-09-19 23:24:25 +0300

    Day 16

---
+++ Day-16/README.md
@@ -0,0 +1,20 @@
+## Day 16 from 100DaysOfCode
+
+### Day 16 Goals
+
+- Object-Oriented Programming (OOP)
+- Python Modules
+- Python Packages
+___
+
+**Day Exercises and Challenges**
+
+- 
+
+**Day's Project is** **.
+
+*Run the file using the following command:*
+
+``` bash
+    python3 
+```
---
+++ Day-16/pretty_table.py
@@ -0,0 +1,17 @@
+#!/uer/bin/python3
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon", ["Pikachu", "Pikachu"])
+table.add_column("Type", ["Electric", "Electric"])
+print(table)
+print()
+
+t = PrettyTable()
+t.field_names = ["Pokemon", "Type"]
+t.add_row(["Pikachu", "Electric"])
+t.add_row(["Pikachu", "Electric"])
+t.add_row(["Pikachu", "Electric"])
+print(t)
+
+
